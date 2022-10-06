51033807a77bb36a22498bf5301d623409bacab2
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-10-06 16:23:13 +0200

    composer e npm i

--- public/js/front.js
+++ public/js/front.js
@@ -54675,7 +54675,7 @@ var routes = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/emanueledezotti/laravel-api/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/emanueledezotti/laravel-boolpress/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
